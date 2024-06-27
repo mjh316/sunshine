a6047a2a7dd4f9b6faa23f336d06f170e7e2522f
Justin Huang <61671361+mjh316@users.noreply.github.com>
2024-06-26 20:27:45 -0700

    feat: added file io for reading in source code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::env;
+
+fn read_file(location: &str) -> String {
+    std::fs::read_to_string(location)
+        .expect("Failed to read file")
+        .trim()
+        .to_string()
+}
+
+#[allow(dead_code)]
+fn write_file(location: &str, data: &str) -> () {
+    std::fs::write(location, data).expect("Failed to write file");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut argv = env::args().collect::<Vec<String>>();
+    let debug = argv.iter().any(|x| x == "--dbg");
+    if debug {
+        argv.retain(|x| x != "--dbg");
+    }
+
+    let location = argv.get(1);
+
+    match location {
+        Some(location) => {
+            println!("Reading file: {}", location);
+            let program = read_file(location);
+            println!("{}", program);
+        }
+        None => {
+            // No file provided, go to REPL?
+            println!("Usage: {} <file>", argv[0]);
+            return;
+        }
+    }
 }
